d8ac93d18244312a94a4ac11ef2bfa5eebe7f57f
Mateo Cano <mcanoa@uqvirtual.edu.co>
2018-05-11 10:54:46 -0500

    terminando seed

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -16,11 +16,21 @@ class UserTableSeeder extends Seeder
             'nombre' => 'Secretaria de Armenia',
         ]);
 
+        DB::table('sede_institucion')->insert([
+            'nombre' => 'Instituto Montenegro',
+            'rector' => 'JOSE URIEL GONZALEZ RAMIREZ',
+            'codigo' => '163470000291',
+            'email' => 'insmontenegro@hotmail.com',
+            'direccion' =>'Carrera 5 #11-07 - Montenegro',
+            'telefono'=>'7535600'
+        ]);
 
          DB::table('users')->insert([
              'nombre_Usuario' => 'Secretaria de Armenia',
              'email' => 'secretaria@gmail.com',
              'password' => bcrypt('secret'),
+             'role'=>'secretaria',
+             'Id_Sede_Institucion'=>1,
              'Id_Secretaria'=>1,
          ]);
     }
